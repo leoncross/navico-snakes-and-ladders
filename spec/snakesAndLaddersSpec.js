13dd8c849d025f8965482e89c99c4a2c7727bc2d
leoncross <leon@lscross.co.uk>
2019-02-11 11:21:18 +0000

    generate board functionality created

--- spec/snakesAndLaddersSpec.js
+++ spec/snakesAndLaddersSpec.js
@@ -6,7 +6,14 @@ describe('Game', function () {
     game = new Game
   })
 
-  describe('#move', function() {
+  describe('#generateBoard', function () {
+    it('creates a 10x10 board', function () {
+      game.generateBoard()
+      expect(game.board.length).toEqual(100)
+    })
+  })
+
+  describe('#move', function () {
     it('moves 1', function () {
       expect(game.move()).toEqual(1)
     })
--- src/snakesAndLadders.js
+++ src/snakesAndLadders.js
@@ -1,4 +1,12 @@
-function Game () {}
+function Game () {
+  this.board = []
+}
+
+Game.prototype.generateBoard = function () {
+  for (var i = 1; i < 101; i++) {
+    this.board.push({ square: i, taken: 0 })
+  }
+}
 
 Game.prototype.move = function () {
   return 1
